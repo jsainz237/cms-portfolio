9f2c6e3226670f3b2a86e24f3c4ca24f4fe36feb
Jesse Sainz <jsainz237@gmail.com>
2025-01-20 18:44:03 -0600

    Fix Project Card scroll/hover issue on mobile

--- @portfolio/portfolio-web/src/components/project-card.tsx
+++ @portfolio/portfolio-web/src/components/project-card.tsx
@@ -17,7 +17,11 @@ interface Props {
 export const ProjectCard = ({ project }: Props) => {
   return (
     <div className="flex flex-col">
-      <div className="group relative flex min-h-[200px] w-full items-center justify-center overflow-hidden rounded-sm bg-background shadow-inset-4 shadow-muted-foreground transition-all duration-300">
+      <div
+        className="group relative flex min-h-[200px] w-full items-center justify-center overflow-hidden rounded-sm bg-background shadow-inset-4 shadow-muted-foreground transition-all duration-300"
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={0} // This allows the card to be focused
+      >
         <div className="relative size-1/2">
           <Image
             src={project.logo!}
@@ -28,11 +32,11 @@ export const ProjectCard = ({ project }: Props) => {
         </div>
         <Suspense fallback={null}>
           <AnimatedFill
-            className="flex flex-col items-center justify-center"
+            className="flex flex-col items-center justify-center group-active:bg-blue-400"
             containerClassName="flex justify-center items-center rounded-sm"
             style={{ backgroundColor: project.background }}
           >
-            <p className="z-20 w-3/4 text-center font-serif font-normal text-background opacity-0 transition-opacity duration-200 group-hover:opacity-100 max-sm:text-sm">
+            <p className="z-20 w-3/4 text-center font-serif font-normal text-background opacity-0 transition-opacity duration-200 group-focus:opacity-100 group-active:opacity-100 max-sm:text-sm [@media(hover:hover)]:group-hover:opacity-100">
               {project.description}
             </p>
           </AnimatedFill>
@@ -49,7 +53,7 @@ export const ProjectCard = ({ project }: Props) => {
             </div>
           ))}
         </div>
-        <div className="max-[560px]:hidden flex gap-2">
+        <div className="flex gap-2 max-[560px]:hidden">
           {project.technologies?.map(tech => (
             <TechIcon key={tech.name} tech={tech} />
           ))}
